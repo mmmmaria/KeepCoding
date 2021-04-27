807ba6e21955fcbd7aa66abe8e705dd68423f49a
Maria Megia <35056750+mmmmaria@users.noreply.github.com>
2021-04-28 00:22:23 +0200

    Add files via upload
    ball con clases

--- k_pygame/ball.py
+++ k_pygame/ball.py
@@ -1,5 +1,16 @@
 import pygame as pg
 import sys
+from random import randint
+
+def rebotaX(x):
+    if x<=0 or x>=ANCHO:
+        return -1
+    return 1
+
+def rebotaY(y):
+    if y<=0 or y>=ALTO:
+        return -1
+    return 1
 
 ROJO = (255,0,0)
 AZUL = (0,0,255)
@@ -10,34 +21,47 @@ ALTO = 600
 
 pg.init()
 pantalla = pg.display.set_mode((ANCHO,ALTO))
-
-game_over = False
-x=ANCHO//2
-y=ALTO//2
-vx=-5
-vy=-5
 reloj = pg.time.Clock() #para ralentizar el movimiento de la bola
 
+class Bola():
+    def __init__(self,x,y,vx,vy,color):
+        self.x=x
+        self.y=y
+        self.vx=vx
+        self.vy=vy
+        self.color=color
+
+bolas=[]
+for _ in range(10):
+    bola = Bola(randint(0,ANCHO),
+            randint(0,ALTO),
+            randint(5,10),
+            randint(5,10),
+            (randint(0,255), randint(0,255), randint(0,255)))
+            
+    bolas.append(bola)
+
+game_over = False
 while not game_over:
-    reloj.tick(60)
+    v=reloj.tick(60)
     #Gestión de eventos
     for evento in pg.event.get():
         if evento.type ==pg.QUIT:
             game_over = True
 
     #Modificación de estado
-    x += vx
-    y += vy
+    for bola in bolas:
+        bola.x += bola.vx
+        bola.y += bola.vy
 
-    if y<=0 or y >= ALTO:
-        vy = -vy #vy*=-1 ó vy=-vy es cambiar el signo
-    
-    if x<=0 or x >= ANCHO:
-        vx = -vx
-    
+        bola.vy *= rebotaY(bola.y)
+        bola.vx *= rebotaX(bola.x)
+
+
     #Gestión de la pantalla
     pantalla.fill(NEGRO)
-    pg.draw.circle(pantalla, ROJO, (x,y),10)
+    for bola in bolas:
+        pg.draw.circle(pantalla, bola.color, (bola.x,bola.y),10)
 
     pg.display.flip()
 
