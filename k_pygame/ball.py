42ba8e8b77c5e06f2e9562c52d465a5d258c0cd5
Maria Megia <35056750+mmmmaria@users.noreply.github.com>
2021-04-29 21:59:54 +0200

    Add files via upload

--- k_pygame/ball.py
+++ k_pygame/ball.py
@@ -1,16 +1,6 @@
 import pygame as pg
 import sys
-from random import randint
-
-def rebotaX(x):
-    if x<=0 or x>=ANCHO:
-        return -1
-    return 1
-
-def rebotaY(y):
-    if y<=0 or y>=ALTO:
-        return -1
-    return 1
+from random import randint, randrange
 
 ROJO = (255,0,0)
 AZUL = (0,0,255)
@@ -24,20 +14,38 @@ pantalla = pg.display.set_mode((ANCHO,ALTO))
 reloj = pg.time.Clock() #para ralentizar el movimiento de la bola
 
 class Bola():
-    def __init__(self,x,y,vx,vy,color):
+    
+    def __init__(self,x,y,vx,vy,color, radio = 10):
         self.x=x
         self.y=y
         self.vx=vx
         self.vy=vy
         self.color=color
+        self.radio = radio
+     
+    def actualizar (self):
+        self.x += self.vx
+        self.y += self.vy
 
+        if self.x<=0 or self.x>=ANCHO:
+            self.vx= -self.vx
+    
+        if self.y<=0 or self.y>=ALTO:
+            self.vy= -self.vy    
+
+    def dibujar(self, lienzo):
+        pg.draw.circle(lienzo, self.color, (self.x, self.y), self.radio)
+
+   
 bolas=[]
 for _ in range(10):
-    bola = Bola(randint(0,ANCHO),
+    bola = Bola(
+            randint(0,ANCHO),
             randint(0,ALTO),
-            randint(5,10),
-            randint(5,10),
-            (randint(0,255), randint(0,255), randint(0,255)))
+            randint(5,10)*randrange(-1,3,2), # *choice([-1,1])
+            randint(5,10)*randrange(-1,3,2),
+            (randint(0,255), randint(0,255), randint(0,255))
+            )
             
     bolas.append(bola)
 
@@ -51,17 +59,14 @@ while not game_over:
 
     #Modificación de estado
     for bola in bolas:
-        bola.x += bola.vx
-        bola.y += bola.vy
-
-        bola.vy *= rebotaY(bola.y)
-        bola.vx *= rebotaX(bola.x)
-
-
+        bola.actualizar()
+             
+        
     #Gestión de la pantalla
     pantalla.fill(NEGRO)
     for bola in bolas:
-        pg.draw.circle(pantalla, bola.color, (bola.x,bola.y),10)
+        bola.dibujar(pantalla)
+        #pg.draw.circle(pantalla, bola.color, (bola.x,bola.y),10)
 
     pg.display.flip()
 
